a1cb840c4acfea36aed23a1d86d5046146dd28e8
Bernola <Bernola@users.noreply.github.com>
2020-10-25 15:18:37 -0300

    Adicionada lógica para mostrar todas as entradas da agenda e o status

--- agenda.js
+++ agenda.js
@@ -1,27 +1,27 @@
 const agendamentos=[{
         data: '13 Jul',
-        horario: '07:30',
+        horario: '8:00',
         nome: 'Guilherme Andrade Silva',
         status: 'agendado',
         medico: 'Fabrício Maciel',
         telefone: '(31) 98346-8649',
     },{
         data: '13 Jul',
-        horario: '08:00',
+        horario: '8:30',
         nome: 'Mateus Morais Dutra',
         status: 'atrasado',
         medico: 'Daniela Trindade',
         telefone: '(31) 98481-6258',
     },{
         data: '13 Jul',
-        horario: '08:30',
+        horario: '9:00',
         nome: 'Abelardo Castro de Araújo Júnior',
         status: 'atrasado',
         medico: 'Daniela Trindade',
         telefone: '(31) 99988-7198',
     },{
         data: '13 Jul',
-        horario: '09:00',
+        horario: '10:00',
         nome: 'Francisvaldo Feitosa da Silva',
         status: 'agendado',
         medico: 'Márcio Rodrigues de Castro',
@@ -29,19 +29,47 @@ const agendamentos=[{
     }
 ]
 
+function adicionaEntrada(estaAgendado, hora, i, j){
+    var entradaAgenda = disponivel.cloneNode(true)
+    if(estaAgendado){
+        entradaAgenda = agendado.cloneNode(true)
+        entradaAgenda.id=i
+        entradaAgenda.querySelector('.dia-mes-agenda').innerText = agendamentos[j].data
+        entradaAgenda.querySelector('.hora-agenda').innerText = agendamentos[j].horario
+        entradaAgenda.querySelector('.nome-agenda').innerText = agendamentos[j].nome
+        entradaAgenda.querySelector('.medico-agenda').innerText = agendamentos[j].medico
+        entradaAgenda.querySelector('.telefone-agenda').innerText = agendamentos[j].telefone
+        entradaAgenda.querySelector('.status-agenda').innerText = agendamentos[j].status
+    }else{
+        entradaAgenda.id=i
+        entradaAgenda.querySelector('.dia-mes-agenda').innerText = '13 Jul'
+        entradaAgenda.querySelector('.hora-agenda').innerText = hora
+        entradaAgenda.querySelector('.disponivel').innerText = 'DISPONÍVEL'
+    }
+    document.getElementById('agenda').appendChild(entradaAgenda)
+}
+
 function mostra(){
     var agendado=document.getElementById('agendado')
     var disponivel=document.getElementById('disponivel')
-    for (var i=0; i<agendamentos.length; i++){
-        var marcacao = agendado.cloneNode(true)
-        marcacao.id=i
-        marcacao.querySelector('.dia-mes-agenda').innerText = agendamentos[i].data
-        marcacao.querySelector('.hora-agenda').innerText = agendamentos[i].horario
-        marcacao.querySelector('.nome-agenda').innerText = agendamentos[i].nome
-        marcacao.querySelector('.medico-agenda').innerText = agendamentos[i].medico
-        marcacao.querySelector('.telefone-agenda').innerText = agendamentos[i].telefone
-        marcacao.querySelector('.status-agenda').innerText = agendamentos[i].status
-        document.getElementById('agenda').appendChild(marcacao)
+    var hora=''
+    var estaAgendado = false
+    for (var i=0; i<20; i++){
+        if(i%2==0){
+            hora=(i/2 + 8) + ':00'
+            console.log(hora)
+        }else{
+            hora=((i-1)/2 + 8) + ':30'
+            console.log(hora)
+        }
+        for(var j=0;j<agendamentos.length;j++){
+            if(hora==agendamentos[j].horario){
+                estaAgendado = true
+                break
+            }
+        }
+        adicionaEntrada(estaAgendado, hora, i, j)
+        estaAgendado=false
     }
     agendado.parentNode.removeChild(agendado);
     disponivel.parentNode.removeChild(disponivel);
